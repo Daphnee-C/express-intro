86dfc78dd7261361189e063d4c7ad5e11ec79ddb
Daphnee-C <dadacc16@gmail.com>
2025-03-04 10:55:50 +0100

    introduction à express

---
+++ index.js
@@ -0,0 +1,46 @@
+import express, { response } from 'express'
+// Import de library express
+
+const app = express()
+// App take everything from express
+
+const movies = [
+    {
+    id : 1,
+    title : `L'amour ouf `,
+    genre : ''
+},
+{
+    id : 2,
+    title : 'Dune 2',
+    genre : 'Schience-Fiction'
+},
+
+{
+    id : 3,
+    title : 'Old boy',
+    genre : 'Thriller'
+},
+{
+    id : 4,
+    title : 'No other land',
+    genre : ''
+}
+
+
+]
+
+app.get('/', (request, response) => {
+    return response.end(`Hello world`)
+})
+// Our first route that takes the path as first parameters, request and response and return a response
+
+app.get('/movies', (request, response) => {
+    return response.json(movies)
+})
+
+
+app.listen(3000, () => {
+    console.log(`server is running on port 3000`)
+})
+// the method listen is used to start our server
